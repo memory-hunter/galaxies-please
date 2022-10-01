8a6ab016c53d98d7d429ba2f0c86495ec279278f
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-10-02 02:34:36 +0400

    Added in music and background

--- src/helpers.py
+++ src/helpers.py
@@ -1,22 +1,38 @@
-from turtle import onclick
 import pygame_menu as pgm
 import constants as c
+
 
 def setup_custom_theme():
     custom_theme = pgm.themes.THEME_DARK.copy()
     custom_theme.title_bar_style = pgm.widgets.MENUBAR_STYLE_NONE
     custom_theme.widget_font = c.BARLOW
     custom_theme.title_font = c.TITILLIUM
-    custom_theme.background_color = (0, 0, 0, 0)
-    custom_theme.title_offset = (18, 0)
+    custom_theme.background_color = pgm.BaseImage(image_path=c.WEBB)
+    custom_theme.title_close_button = False
+    custom_theme.widget_background_color = (30, 30, 30, 200)
+    custom_theme.title_offset = (c.WIDTH/2.6, 0)
 
     return custom_theme
 
+
+def toggle_music(menu):
+    c.MUSIC = not c.MUSIC
+    menu.get_widget("Music").set_title(
+        "Music: " + ("On" if c.MUSIC else "Off"))
+
+
 def setup_main_menu():
-    menu = pgm.Menu(c.TITLE, c.WIDTH/2, c.HEIGHT/2, theme=setup_custom_theme(), center_content=True, mouse_enabled=True, mouse_motion_selection=True, mouse_visible=True, onclose=pgm.events.CLOSE)
+    menu = pgm.Menu(c.TITLE, c.WIDTH, c.HEIGHT,
+                    theme=setup_custom_theme(),
+                    center_content=True,
+                    mouse_enabled=True,
+                    mouse_motion_selection=True,
+                    mouse_visible=True,
+                    onclose=pgm.events.CLOSE
+                    )
     menu.add.button("Play", pgm.events.CLOSE)
-    menu.add.button("Settings")
+    menu.add.button("Music: " + ("On" if c.MUSIC else "Off"),
+                    lambda: toggle_music(menu), button_id="Music")
     menu.add.button("Quit", pgm.events.EXIT)
 
     return menu
-    
