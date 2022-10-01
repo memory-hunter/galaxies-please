ba13022fdcec8262e14cba7e5521e944ab42ebaa
ALXSAI <alexandersainishvili@gmail.com>
2022-10-01 15:42:46 +0400

    created document class to handle stellar objects

---
+++ src/documents/document.py
@@ -0,0 +1,55 @@
+import random
+
+
+class Document:
+    def __init__(self, data, false_data):
+        self.name = data["name"]
+        self.img = data["img"]
+        self.distance = data["distance"]
+        self.type = data["type"]
+        self.luminosity = data["luminosity"]
+        self.size = data["size"]
+        self.doctype = data["doctype"]
+        self.false_data = false_data
+        self.fact = True
+
+    def present(self):
+        falsefy = random.randint(0,5)
+        if falsefy > 3:
+            self.falsifyinfo()
+        return self
+
+    def falsifyinfo(self):
+        # 0-img, 1-distance, 2-type, 3-luminosity, 4-size
+        falsifyindex = [random.randint(1, 2), random.randint(1, 2), random.randint(1, 2), random.randint(1, 2),
+                        random.randint(1, 2)]
+        amountfalsified = 0
+
+        if falsifyindex[0] == 2:
+            temp = self.false_data["img"]
+            self.img = temp
+            amountfalsified += 1
+
+        if falsifyindex[1] == 2 and self.distance != self.false_data["distance"]:
+            temp = self.false_data["distance"]
+            self.distance = temp
+            amountfalsified += 1
+
+        if falsifyindex[2] == 2 and self.type != self.false_data["type"]:
+            self.type = self.false_data["type"]
+            amountfalsified += 1
+
+        if falsifyindex[3] == 2 and self.luminosity != self.false_data["luminosity"]:
+            self.luminosity = self.false_data["luminosity"]
+            amountfalsified += 1
+
+        if falsifyindex[4] == 2 and self.size != self.false_data["size"]:
+            self.size = self.false_data["size"]
+            amountfalsified += 1
+
+        if amountfalsified > 0:
+            self.fact = False
+
+    def arrayfy(self):
+        return [self.name,self.img,self.distance,self.type,self.luminosity,self.size,self.doctype,self.fact]
+
