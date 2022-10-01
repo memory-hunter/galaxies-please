e2b49ec1dfb631fc31b521331afd9589334644cc
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-10-02 02:35:04 +0400

    Fully working game menu with background and music

--- src/main.py
+++ src/main.py
@@ -1,38 +1,53 @@
 import sys
 import pygame as pg
 from pygame.locals import *
+from pygame import mixer
 import constants as c
 import helpers as h
 
-def runpg():
-  pg.init()
-  fpsClock = pg.time.Clock()
 
-  screen = pg.display.set_mode((c.WIDTH, c.HEIGHT))
-  pg.display.set_caption(c.TITLE)
-  pg.display.set_icon(pg.image.load(c.ICON))
+def run_game():
+    pg.init()
+    mixer.init()
 
-  main_menu = h.setup_main_menu()
-  main_menu.mainloop(screen)
+    screen = pg.display.set_mode((c.WIDTH, c.HEIGHT))
+    pg.display.set_caption(c.TITLE)
+    pg.display.set_icon(pg.image.load(c.ICON))
 
-  while True:
-    # Update
-    events = pg.event.get()
-    if main_menu.is_enabled():
-        main_menu.update(events)
+    main_menu = h.setup_main_menu()
 
-    for event in events:
-      if event.type == QUIT:
-        pg.quit()
-        sys.exit()
-    
-    # Draw
-    screen.fill((0, 0, 0))
+    mixer.music.load(c.MMBG)
+    mixer.music.play(-1, fade_ms=500)
+    mixer.music.set_volume(0.5)
 
-    if main_menu.is_enabled():
-        main_menu.draw(screen)
-        
-    pg.display.flip()
+    while True:
+        # Update
+        events = pg.event.get()
+        if main_menu.is_enabled():
+            if (c.MUSIC):
+                mixer.music.unpause()
+            else:
+                mixer.music.pause()
+            main_menu.update(events)
+        else:
+            mixer.music.fadeout(500)
+            mixer.music.load(c.IGBG)
+            mixer.music.play(-1, fade_ms=500)
+            mixer.music.set_volume(0.5)
+
+        for event in events:
+            if event.type == QUIT:
+                pg.quit()
+                sys.exit()
+
+        # Draw
+        screen.fill((0, 0, 0))
+
+        if main_menu.is_enabled():
+            main_menu.draw(screen)
+
+        pg.display.flip()
+
 
 if __name__ == '__main__':
-  runpg()
+    run_game()
