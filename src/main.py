3b4bd3f27635225d8a98d083343a52d7d8fd9ba2
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-10-02 06:51:28 +0400

    Dummy screen for main game

--- src/main.py
+++ src/main.py
@@ -4,7 +4,36 @@ from pygame.locals import *
 from pygame import mixer
 import constants as c
 import helpers as h
+from components import button as b
 
+
+def play_state(screen, items):
+    for item in items:
+        if isinstance(item, b.Button):
+            item.draw(screen)
+        else:
+            screen.blit(item, (0, 0))
+
+def change_music():
+    if(c.MUSIC):
+        if c.TRACKSTATE == -1:
+            c.TRACKSTATE = 0
+            mixer.music.load(c.MMBG)
+            mixer.music.play(-1, fade_ms=500)
+            mixer.music.set_volume(0.5)
+
+        elif c.TRACKSTATE == 0:
+            c.TRACKSTATE = 1
+            mixer.music.fadeout(500)
+            mixer.music.load(c.IGBG)
+            mixer.music.play(-1, fade_ms=500)
+
+        elif c.TRACKSTATE == 1:
+            c.TRACKSTATE = 0
+            mixer.music.fadeout(500)
+            mixer.music.load(c.MMBG)
+            mixer.music.play(-1, fade_ms=500)
+            mixer.music.set_volume(0.5)
 
 def run_game():
     pg.init()
@@ -15,10 +44,14 @@ def run_game():
     pg.display.set_icon(pg.image.load(c.ICON))
 
     main_menu = h.setup_main_menu()
+    
+    table = pg.image.load(c.WORKTABLE).convert_alpha()
+    table = pg.transform.scale(table, (c.WIDTH, c.HEIGHT))
 
-    mixer.music.load(c.MMBG)
-    mixer.music.play(-1, fade_ms=500)
-    mixer.music.set_volume(0.5)
+    button = b.Button(c.WIDTH/1.175, c.HEIGHT/2, pg.image.load(c.WEBBBUTTON).convert_alpha(), 0.4)
+
+    change_music()
+    c.GAMESTATE = 0
 
     while True:
         # Update
@@ -30,10 +63,9 @@ def run_game():
                 mixer.music.pause()
             main_menu.update(events)
         else:
-            mixer.music.fadeout(500)
-            mixer.music.load(c.IGBG)
-            mixer.music.play(-1, fade_ms=500)
-            mixer.music.set_volume(0.5)
+            if c.GAMESTATE == 0:
+                change_music()
+                c.GAMESTATE = 1
 
         for event in events:
             if event.type == QUIT:
@@ -45,9 +77,10 @@ def run_game():
 
         if main_menu.is_enabled():
             main_menu.draw(screen)
+        else:
+            play_state(screen, [table, button])
 
         pg.display.flip()
-
 
 if __name__ == '__main__':
     run_game()
